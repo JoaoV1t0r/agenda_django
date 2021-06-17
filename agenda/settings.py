56e79d70e2017adf516056bb5e54744de17abd94
JoaoV1t0r <joaovitor.silva2pereira@gmail.com>
2021-06-17 15:03:12 -0300

    Adionando busca e foto nos contatos

--- agenda/settings.py
+++ agenda/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vx3@q5080py-kr*knj@73r@t+u6!@9j+a%s2-gbmj-5hvdjaj_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -132,3 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'templates/static')
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
--- contatos/views.py
+++ contatos/views.py
@@ -4,6 +4,8 @@ from django.http.response import HttpResponse
 from django.core.paginator import Paginator
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 
 def index(request):
@@ -23,8 +25,23 @@ def ver_contato(request, contato_id):
         return render(request, 'contatos/ver_contato.html', {'contato': contato})
     else:
         raise Http404()
-    # try:
-    #     contato = Contato.objects.get(id=contato_id)
-    #     return render(request, 'contatos/ver_contato.html', {'contato': contato})
-    # except Contato.DoesNotExist as e:
-    #     raise Http404()
+
+
+def busca(request):
+    termo = request.GET.get('termo')
+
+    if termo is None:
+        raise Http404()
+
+    campos = Concat('nome', Value(' '), 'sobrenome')
+    contatos = Contato.objects.annotate(
+        nome_completo=campos
+    ).filter(
+        Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo),
+        mostrar=True
+    )
+    paginator = Paginator(contatos, 1)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+    return render(request, 'contatos/busca.html', {'contatos': contatos})
