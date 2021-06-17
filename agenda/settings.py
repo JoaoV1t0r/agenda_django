5e9434375b9a46a9d863adfa03f44cef95d1ac77
JoaoV1t0r <joaovitor.silva2pereira@gmail.com>
2021-06-17 13:38:41 -0300

    adicionando paginação

--- agenda/settings.py
+++ agenda/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vx3@q5080py-kr*knj@73r@t+u6!@9j+a%s2-gbmj-5hvdjaj_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- contatos/views.py
+++ contatos/views.py
@@ -1,13 +1,30 @@
+from django.core import paginator
 from .models import Contato
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('-id').filter(
+        mostrar=True
+    )
+    paginator = Paginator(contatos, 1)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
     return render(request, 'contatos/index.html', {'contatos': contatos})
 
 
 def ver_contato(request, contato_id):
-    contato = Contato.objects.get(id=contato_id)
-    return render(request, 'contatos/ver_contato.html', {'contato': contato})
+    contato = get_object_or_404(Contato, id=contato_id)
+    if contato.mostrar:
+        return render(request, 'contatos/ver_contato.html', {'contato': contato})
+    else:
+        raise Http404()
+    # try:
+    #     contato = Contato.objects.get(id=contato_id)
+    #     return render(request, 'contatos/ver_contato.html', {'contato': contato})
+    # except Contato.DoesNotExist as e:
+    #     raise Http404()
