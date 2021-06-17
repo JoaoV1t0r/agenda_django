c16e358c0d69e99218c316226e3abb2471dc654a
JoaoV1t0r <joaovitor.silva2pereira@gmail.com>
2021-06-17 17:45:12 -0300

    adicionado registro de usuário

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
 import accounts
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.core.validators import validate_email
+from django.contrib.auth.models import User
 
 
 def login(request):
@@ -11,7 +14,44 @@ def logout(request):
 
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method != 'POST':
+        return render(request, 'accounts/register.html')
+
+    nome = request.POST.get('nome')
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+    confirma_senha = request.POST.get('confirma_senha')
+
+    if not nome or not email or not senha or not confirma_senha:
+        messages.error(request, 'Nenhum campo poder estar vazio')
+        return render(request, 'accounts/register.html')
+
+    try:
+        validate_email(email)
+    except:
+        messages.error(request, 'E-mail inválido')
+        return render(request, 'accounts/register.html')
+
+    if len(nome) < 6:
+        messages.error(request, 'Nome precisa ter no mínimo 6 caracteres')
+        return render(request, 'accounts/register.html')
+
+    if len(senha) < 4:
+        messages.error(request, 'Senha precisa ter no mínimo 4 caracteres')
+        return render(request, 'accounts/register.html')
+
+    if senha != confirma_senha:
+        messages.error(request, 'As senha são diferentes, tente novamente')
+        return render(request, 'accounts/register.html')
+
+    exists = User.objects.filter(email=email)
+    if exists:
+        messages.error(request, 'E-mail já cadastrado')
+        return render(request, 'accounts/register.html')
+
+    user = User.objects.create_user(username=nome, email=email, password=senha)
+    user.save()
+    return redirect('login')
 
 
 def dashboard(request):
